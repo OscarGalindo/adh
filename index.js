d0934c93709f2574a49abd87a2bbc9b43db8cb38
OscarGalindo <oscar.galindo@apiumhub.com>
2017-07-13 23:50:57 +0200

    Default console log out of stdExec

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 'use strict';
 const program = require('commander');
 const chalk = require("chalk");
+const boxen = require("boxen");
 const action = require('./action');
 const pkg = require('./package.json');
 const simpleCommands = require('./commands.json');
@@ -15,10 +16,14 @@ simpleCommands.forEach(command => {
     .command(command.cli)
     .alias(command.alias)
     .description(chalk.yellow(command.description))
-    .action(action.stdExec.bind(null, {
-      cmd: command.cmd,
-      color: typeof command.color === 'undefined' ? true : command.color
-    }))
+    .action(() => {
+      action.stdExec({
+        cmd: command.cmd,
+        color: typeof command.color === 'undefined' ? true : command.color
+      }).then(x => {
+        console.log(boxen(x, {padding: 1, borderColor: "yellow"}));
+      })
+    })
 });
 
 program.on('*', function () {
