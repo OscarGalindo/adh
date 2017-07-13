3c38a4c0887ad8dc97a3fa32b3bb1874911f40bd
OscarGalindo <oscar.galindo@apiumhub.com>
2017-07-13 23:46:39 +0200

    Fix nginx

--- action.js
+++ action.js
@@ -2,7 +2,7 @@ const chalk = require("chalk");
 const execa = require('execa');
 
 module.exports.stdExec = (action) => {
-  execa.shell(action.cmd)
-    .then(res => console.log(action.color ? chalk.green(res.stdout) : res.stdout))
+  return execa.shell(action.cmd)
+    .then(res => res.stdout)
     .catch(err => console.log(chalk.red("stdExec error: " + err)));
 };
